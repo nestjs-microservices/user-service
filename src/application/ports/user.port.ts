fad6eabc980078a99835b97315b868dba518c52f
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2022-02-13 01:55:57 +0300

    feat: Implementing user port

---
+++ src/application/ports/user.port.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../domain/services/prisma/prisma.service';
+import { UserEntity } from '../../domain/entities/user.entity';
+import { CreateUserArgs } from '../../presentation/controllers/user.controller';
+
+@Injectable()
+export class UserPort {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createUser(user: CreateUserArgs) {
+    const { email, password } = user;
+    const { id } = await this.prismaService.user.create({
+      data: { email, credentials: { create: { password } } },
+    });
+    return new UserEntity(id, email, password);
+  }
+}
