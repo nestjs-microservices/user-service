b7f5a8eeaddd77fa452a99abc10fee5f47d3906d
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2022-02-13 01:57:04 +0300

    feat: providing newly created classes

--- src/presentation/presentation.module.ts
+++ src/presentation/presentation.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './controllers/app.controller';
 import { AppService } from '../domain/services/app.service';
+import { UserController } from './controllers/user.controller';
+import { CreateUserUseCase } from '../application/use-cases/user/create-user.use-case';
+import { UserPort } from '../application/ports/user.port';
+import {PrismaService} from "../domain/services/prisma/prisma.service";
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UserController],
+  providers: [AppService, CreateUserUseCase, UserPort, PrismaService],
 })
 export class PresentationModule {}
