54278a8b8b5ea801b0953f6be61542ec15b5d074
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2022-02-12 22:06:34 +0300

    feat: updating applicaiton architecture

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
-import { AppModule } from './app.module';
+import { PresentationModule } from './presentation/presentation.module';
 import { Logger } from '@nestjs/common';
 
 const logger = new Logger();
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(AppModule, {
+  const app = await NestFactory.createMicroservice(PresentationModule, {
     transport: Transport.TCP,
     options: {
       host: '0.0.0.0',
