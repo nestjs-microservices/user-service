7a0974221a97725ac6e1583a3fe07995af591e69
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2022-02-13 23:37:08 +0300

    feat: providing transport interceptors

--- src/presentation/presentation.module.ts
+++ src/presentation/presentation.module.ts
@@ -6,13 +6,20 @@ import { CreateUserUseCase } from '../application/use-cases/user/create-user.use
 import { UserPort } from '../application/ports/user.port';
 import { PrismaService } from '../domain/services/prisma.service';
 import { GetUserUseCase } from '../application/use-cases/user/get-user.use-case';
+import { LoggerService } from '../domain/services/logger.service';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { TransportInterceptor } from '../application/interceptors/transport.interceptor';
 
 const USE_CASES = [CreateUserUseCase, GetUserUseCase];
 const PORTS = [UserPort];
+const SERVICES = [PrismaService, LoggerService];
+const TRANSPORTS = [
+  { provide: APP_INTERCEPTOR, useClass: TransportInterceptor },
+];
 
 @Module({
   imports: [],
   controllers: [AppController, UserController],
-  providers: [AppService, ...USE_CASES, ...PORTS, PrismaService],
+  providers: [AppService, ...USE_CASES, ...PORTS, ...SERVICES, ...TRANSPORTS],
 })
 export class PresentationModule {}
