55b02af5de6054c419a9fbf09347a3b588974f4d
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2022-02-13 23:33:30 +0300

    feat: adding error handler for all methods

--- src/application/ports/user.port.ts
+++ src/application/ports/user.port.ts
@@ -1,27 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from '../../domain/services/prisma.service';
 import { UserEntity } from '../../domain/entities/user.entity';
 import { CreateUserArgs } from '../../presentation/controllers/user.controller';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class UserPort {
   constructor(private readonly prismaService: PrismaService) {}
 
   async createUser(user: CreateUserArgs) {
-    const { email, password } = user;
-    const { id } = await this.prismaService.user.create({
-      data: { email, credentials: { create: { password } } },
-    });
-    return new UserEntity(id, email, password);
+    try {
+      const { email, password } = user;
+      const { id } = await this.prismaService.user.create({
+        data: { email, credentials: { create: { password } } },
+      });
+      return new UserEntity(id, email, password);
+    } catch (e) {
+      throw new RpcException({
+        message: e.message,
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+      });
+    }
   }
 
-  async getUser(email: string) {
-    const user = await this.prismaService.user.findUnique({ where: { email } });
-    return new UserEntity(user.id, user.email, '');
+  async getUserByEmail(email: string) {
+    let user;
+    try {
+      user = await this.prismaService.user.findUnique({ where: { email } });
+      return new UserEntity(user.id, user.email, '');
+    } catch (e) {
+      throw new RpcException({
+        message: `User with email=${email} not found`,
+        statusCode: HttpStatus.NOT_FOUND,
+      });
+    }
   }
 
   async getUserById(id: number) {
-    const user = await this.prismaService.user.findUnique({ where: { id } });
-    return new UserEntity(user.id, user.email, '');
+    let user;
+    try {
+      user = await this.prismaService.user.findUnique({ where: { id } });
+      return new UserEntity(user.id, user.email, '');
+    } catch (e) {
+      throw new RpcException({
+        message: `User with id=${id} not found`,
+        statusCode: HttpStatus.NOT_FOUND,
+      });
+    }
   }
 }
