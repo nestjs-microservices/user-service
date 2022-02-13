8b5672a2c536fca08fcf9be85369bc7ea5f48ea3
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2022-02-13 16:30:13 +0300

    feat: implementing get user

--- src/application/ports/user.port.ts
+++ src/application/ports/user.port.ts
@@ -14,4 +14,13 @@ export class UserPort {
     });
     return new UserEntity(id, email, password);
   }
+
+  async getUser(email: string) {
+    const user = await this.prismaService.user.findUnique({ where: { email } });
+    const { password } = await this.prismaService.userCredential.findFirst({
+      where: { user_id: user.id },
+      orderBy: { created_at: 'desc' },
+    });
+    return new UserEntity(user.id, user.email, password);
+  }
 }
