65a55199967e9f268706a2ea98bfa64248466e8f
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2022-02-13 16:33:00 +0300

    feat: implementing get user use case

--- src/application/use-cases/user/create-user.use-case.ts
+++ src/application/use-cases/user/create-user.use-case.ts
@@ -1,14 +1,12 @@
 import { UseCase } from '../../../use-case';
-import { UserEntity } from '../../../domain/entities/user.entity';
 import { Injectable } from '@nestjs/common';
 import { UserPort } from '../../ports/user.port';
 import { CreateUserArgs } from '../../../presentation/controllers/user.controller';
 
 @Injectable()
-export class CreateUserUseCase implements UseCase<UserEntity> {
-  constructor(
-    private userPort: UserPort,
-  ) {}
+export class CreateUserUseCase implements UseCase {
+  constructor(private userPort: UserPort) {}
+
   exec(user: CreateUserArgs) {
     return this.userPort.createUser(user);
   }
---
+++ src/application/use-cases/user/get-user.use-case.ts
@@ -0,0 +1,12 @@
+import { UseCase } from '../../../use-case';
+import { Injectable } from '@nestjs/common';
+import { UserPort } from '../../ports/user.port';
+
+@Injectable()
+export class GetUserUseCase implements UseCase {
+  constructor(private userPort: UserPort) {}
+
+  exec(email: string) {
+    return this.userPort.getUser(email);
+  }
+}
