dd0e6fe3408bc539a3469f407cafdd84dd49f4d9
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2022-02-13 23:38:45 +0300

    feat: providing custom exception filter

--- src/presentation/presentation.module.ts
+++ src/presentation/presentation.module.ts
@@ -7,8 +7,9 @@ import { UserPort } from '../application/ports/user.port';
 import { PrismaService } from '../domain/services/prisma.service';
 import { GetUserUseCase } from '../application/use-cases/user/get-user.use-case';
 import { LoggerService } from '../domain/services/logger.service';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { TransportInterceptor } from '../application/interceptors/transport.interceptor';
+import { ExceptionFilter } from '../application/filters/exception.filter';
 
 const USE_CASES = [CreateUserUseCase, GetUserUseCase];
 const PORTS = [UserPort];
@@ -16,10 +17,18 @@ const SERVICES = [PrismaService, LoggerService];
 const TRANSPORTS = [
   { provide: APP_INTERCEPTOR, useClass: TransportInterceptor },
 ];
+const FILTERS = [{ provide: APP_FILTER, useClass: ExceptionFilter }];
 
 @Module({
   imports: [],
   controllers: [AppController, UserController],
-  providers: [AppService, ...USE_CASES, ...PORTS, ...SERVICES, ...TRANSPORTS],
+  providers: [
+    AppService,
+    ...USE_CASES,
+    ...PORTS,
+    ...SERVICES,
+    ...TRANSPORTS,
+    ...FILTERS,
+  ],
 })
 export class PresentationModule {}
