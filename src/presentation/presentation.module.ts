b0b3fcabd9c858ebd4569851b9544e27dfa2f57e
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2022-02-13 16:38:01 +0300

    fix: refactoring providers

--- src/presentation/presentation.module.ts
+++ src/presentation/presentation.module.ts
@@ -7,15 +7,12 @@ import { UserPort } from '../application/ports/user.port';
 import { PrismaService } from '../domain/services/prisma.service';
 import { GetUserUseCase } from '../application/use-cases/user/get-user.use-case';
 
+const USE_CASES = [CreateUserUseCase, GetUserUseCase];
+const PORTS = [UserPort];
+
 @Module({
   imports: [],
   controllers: [AppController, UserController],
-  providers: [
-    AppService,
-    CreateUserUseCase,
-    GetUserUseCase,
-    UserPort,
-    PrismaService,
-  ],
+  providers: [AppService, ...USE_CASES, ...PORTS, PrismaService],
 })
 export class PresentationModule {}
